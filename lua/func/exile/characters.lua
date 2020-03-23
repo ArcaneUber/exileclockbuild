0cdc89d2228db680ba4265b92c5869f9b8b7ae63
ArcaneUber <47839522+ArcaneUber@users.noreply.github.com>
2020-03-23 15:11:50 -0400

    Add files via upload

---
+++ lua/func/exile/characters.lua
@@ -0,0 +1,41 @@
+
+---- RACHEL
+
+
+do
+Rachel_Char = {
+	PlayerType = Isaac.GetPlayerTypeByName("Rachel"),
+	HairGfx = Isaac.GetCostumeIdByPath("gfx/characters/rachel_hair.anm2"),
+	BodyGfx = Isaac.GetCostumeIdByPath("gfx/characters/rachel_body.anm2"),
+	UnlockGFX = {
+		ItLives = "",
+		Isaac = "gfx/ui/achievement/custom/ach_heresy.png",
+		Satan = "",
+		MegaSatan = "",
+		TheLamb = "gfx/ui/achievement/custom/ach_lectionary.png",
+		Hush = "",
+		Delirium = "",
+		UltraGreed = "",
+		BossRush = "gfx/ui/achievement/custom/ach_corinthcloak.png",
+		BlueBaby = "",
+	},
+}
+
+end
+
+function EXILE_M:OnRenderRachel()
+local player = Isaac.GetPlayer(0)
+if player:GetPlayerType() == Rachel_Char.PlayerType then
+	player:AddNullCostume(Rachel_Char.HairGfx)
+	player:AddNullCostume(Rachel_Char.BodyGfx)
+	if EXILE_M.SavedData.InitRachel == 0 then
+		player:AddCollectible(EXILE_M.COLLECTIBLE_CROSIER, 0, false)
+		EXILE_M.SavedData.InitRachel = 1
+	end
+
+end
+end
+
+EXILE_M:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, EXILE_M.OnRenderRachel)
+
+EXILE_M.bork.NonExistantFunctionThatIsCalledToIntentionallyErrorThis()
